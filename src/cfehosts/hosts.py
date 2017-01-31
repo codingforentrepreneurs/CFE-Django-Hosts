bdf1f245381b35fde63e92951d6827c5d9cca65c
CFE <hello@teamcfe.com>
2017-01-30 16:31:09 -0800

    5 - Routing Subdomain Names

--- src/cfehosts/hosts.py
+++ src/cfehosts/hosts.py
@@ -4,6 +4,7 @@ from django_hosts import patterns, host
 
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
+    host(r'admin', 'cfehosts.blog_urls', name='admin'),
     #host(r'(\w+)', 'path.to.custom_urls', name='wildcard'),
 )
 
